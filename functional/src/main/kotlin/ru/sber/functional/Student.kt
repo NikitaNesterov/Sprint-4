7ea6997768fd197285f05a465504c4f70e7f7947
Nikita <59733197+NikitaNesterov@users.noreply.github.com>
2021-10-01 13:29:23 +0300

    ДЗ functional

--- functional/src/main/kotlin/ru/sber/functional/Student.kt
+++ functional/src/main/kotlin/ru/sber/functional/Student.kt
@@ -3,10 +3,10 @@ package ru.sber.functional
 data class Student(
     val firstName: String,
     val lastName: String,
-    val middleName: String,
-    val age: Int,
+    val middleName: String = "Отсутствует",
+    val age: Int = 99,
     val averageRate: Double,
-    val city: String,
-    val specialization: String,
-    val prevEducation: String?,
+    val city: String = "Где-то в России",
+    val specialization: String = "Пок аотсутствует",
+    val prevEducation: String? = "Школа",
 )
--- functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
+++ functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
@@ -4,7 +4,19 @@ class StudentsGroup {
 
     lateinit var students: List<Student>
 
-    fun filterByPredicate() {
-        TODO()
+    fun init(_firstName: List<String>, _lastNJame: List<String>, _averageRate: List<Double>, studentsQuantity: Int) {
+
+        students = ArrayList<Student>()
+        for(i in 0 until studentsQuantity) {
+            (students as ArrayList<Student>).add(Student(_firstName[i], _lastNJame[i], averageRate = _averageRate[i]))
+        }
     }
+
+    fun filterByPredicate(predicate: (Student) -> Boolean) : ArrayList<Student> {
+        return students.filterTo(ArrayList<Student>(), predicate)
+    }
+
 }
+
+
+
